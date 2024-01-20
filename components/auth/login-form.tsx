8a13fa959f738a1eefed3cad822b53dc4570f899
Nursandi <sandinur157@gmail.com>
2024-01-20 12:38:20 +0700

    feat: add login dialog and callback url

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -25,6 +25,7 @@ import Link from 'next/link'
 
 export default function LoginForm() {
   const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('callbackUrl')
   const urlError =
     searchParams.get('error') === 'OAuthAccountNotLinked'
       ? 'Email already in use with different provider!'
@@ -48,7 +49,7 @@ export default function LoginForm() {
     setSuccess('')
 
     startTransition(() => {
-      login(values)
+      login(values, callbackUrl)
         .then((data) => {
           if (data?.error) {
             setError(data.error)
